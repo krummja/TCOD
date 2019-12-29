aaa4424bdb677983525697ba5ecc6af7ab77e746
Jonathan Crum <crumja@uga.edu>
2019-12-29 00:15:04 -0500

    Render functions

--- engine.py
+++ engine.py
@@ -1,43 +1,46 @@
 import tcod
+
+from entity import Entity
 from input_handlers import handle_keys
+from render_functions import clear_all, render_all
 
 def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    #* Entities
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', tcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', tcod.yellow)
+    entities = [npc, player]
 
+    #* Controls
     key = tcod.Key()
     mouse = tcod.Mouse()
 
+    #* Console
     tcod.console_set_custom_font('./assets/arial10x10.png', tcod.FONT_TYPE_GRAYSCALE | tcod.FONT_LAYOUT_TCOD)
-
     tcod.console_init_root(screen_width, screen_height, 'tcod tutorial', False)
-
     con = tcod.console_new(screen_width, screen_height)
 
     #! Main Game Loop
     while not tcod.console_is_window_closed():
         tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
-
-        tcod.console_set_default_foreground(con, tcod.white)
-        tcod.console_put_char(con, player_x, player_y, '@', tcod.BKGND_NONE)
-        tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+        
+        #* Drawing Functions
+        render_all(con, entities, screen_width, screen_height)
         tcod.console_flush()
-
-        tcod.console_put_char(con, player_x, player_y, ' ', tcod.BKGND_NONE)
-
+        clear_all(con, entities)
+        
+        #* Input Functions
         action = handle_keys(key)
-
         move = action.get('move')
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        #* Control Logic
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            player.move(dx, dy)
 
         if exit:
             return True
---
+++ render_functions.py
@@ -0,0 +1,18 @@
+import tcod
+
+def render_all(con, entities, screen_width, screen_height):
+    for entity in entities:
+        draw_entity(con, entity)
+    
+    tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+def clear_all(con, entities):
+    for entity in entities:
+        clear_entity(con, entity)
+
+def draw_entity(con, entity):
+    tcod.console_set_default_foreground(con, entity.color)
+    tcod.console_put_char(con, entity.x, entity.y, entity.char, tcod.BKGND_NONE)
+
+def clear_entity(con, entity):
+    tcod.console_put_char(con, entity.x, entity.y, ' ', tcod.BKGND_NONE)
