7f8f73bcbfb5230d145e93897273cae0acab0fd7
Jonathan Crum <crumja@uga.edu>
2020-01-13 20:54:33 -0500

    End of Part 8

--- input_handlers.py
+++ input_handlers.py
@@ -1,8 +1,21 @@
 import tcod
 
+from game_states import GameStates
 
 
-def handle_keys(key):
+def handle_keys(key, game_state):
+    if game_state == GameStates.PLAYERS_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        return handle_inventory_keys(key)
+
+    return {}
+
+
+def handle_player_turn_keys(key):
+    key_char = chr(key.c)
 
     if key.vk == tcod.KEY_KP8:
         return {'move': (0, -1)}
@@ -21,8 +34,14 @@ def handle_keys(key):
     elif key.vk == tcod.KEY_KP3:
         return {'move': (1, 1)}
 
-    if key.char == 'g':
+    if key_char == 'd':
+        return {'drop_inventory': True}
+
+    if key_char == 'g':
         return {'pickup': True}
+
+    elif key_char == 'i':
+        return {'show_inventory': True}
     
     if key.vk == tcod.KEY_ENTER and key.lalt:
         return {'fullscreen': True}
@@ -32,3 +51,31 @@ def handle_keys(key):
     
     return {}
 
+
+def handle_player_dead_keys(key):
+    key_char = chr(key.c)
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+
+    if key.vk == tcod.KEY_ENTER and key.lalt:
+        return {'fullscreen': True}
+    
+    elif key.vk == tcod.KEY_ESCAPE:
+        return {'exit': True}
+    
+    return {}
+
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
+
+    if key.vk == tcod.KEY_ENTER and key.lalt:
+        return {'fullscreen': True}
+    elif key.vk == tcod.KEY_ESCAPE:
+        return {'exit': True}
+    
+    return {}
